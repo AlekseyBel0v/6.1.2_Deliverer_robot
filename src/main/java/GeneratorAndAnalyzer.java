465f863a0c5f1bd564201c697e672a2e1fecda4e
Alexey Belov <bel0valeksey@yandex.ru>
2023-03-25 05:25:44 +0300

    Add freqLog branch

--- src/main/java/GeneratorAndAnalyzer.java
+++ src/main/java/GeneratorAndAnalyzer.java
@@ -1,11 +1,14 @@
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.TreeMap;
 
 public class GeneratorAndAnalyzer {
     static final Map<Integer, Integer> sizeToFreq = new TreeMap<>(Integer::compareTo);
+    static Boolean mapIsUpdated = false;
 
+    /**
+     * Метод генерирует строку, состоящую из набора символов
+     */
     public static String generateRoute(String letters, int length) {
         Random random = new Random();
         StringBuilder route = new StringBuilder();
@@ -15,6 +18,9 @@ public class GeneratorAndAnalyzer {
         return route.toString();
     }
 
+    /**
+     * Метод анализирует строку, выводит на экран ее и сколько определенных сиволов она содержит.
+     */
     public static void analyzeRoute(String rout, char letterForAnalysis) {
         int quantity = 0;
         for (int i = 0; i < Main.LENGTHS_OF_THE_ROUT; i++) {
@@ -22,13 +28,21 @@ public class GeneratorAndAnalyzer {
                 quantity++;
             }
         }
-        synchronized(sizeToFreq) {
+        synchronized (sizeToFreq) {
+            if (mapIsUpdated) {
+                try {
+                    sizeToFreq.wait();
+                } catch (InterruptedException e) {
+                    return;
+                }
+            }
             if (!sizeToFreq.containsKey(quantity)) {
                 sizeToFreq.put(quantity, 1);
             } else {
                 sizeToFreq.put(quantity, sizeToFreq.get(quantity) + 1);
             }
+            System.out.println("Путь: " + rout + "; Количество \"" + Main.LETTER_FOR_ANALYSIS + "\" = " + quantity);
+            mapIsUpdated = true;
         }
-        System.out.println("Путь: " + rout + "; Количество \"" + Main.LETTER_FOR_ANALYSIS + "\" = " + quantity);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,47 @@
-import javax.lang.model.element.Element;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
-    static final int QUANTITY_OF_ROUTS_VARIANTS = 1000;
-    static final String LETTERS = "RLRFR";
-    static final int LENGTHS_OF_THE_ROUT = 100;
-    static final char LETTER_FOR_ANALYSIS = 'R';
+    static final int QUANTITY_OF_ROUTS_VARIANTS = 1000;     // каждый вариант обрабабтывает отдельный поток
+    static final String LETTERS = "RLRFR";                  // из чего состоит "путь"
+    static final int LENGTHS_OF_THE_ROUT = 100;             // длина пути
+    static final char LETTER_FOR_ANALYSIS = 'R';            // на эту букву будет проанализирован путь
+    static Thread currentHigh = new Thread(() -> {
+        while (true) {
+            while (!GeneratorAndAnalyzer.mapIsUpdated) {
+            }
+            synchronized (GeneratorAndAnalyzer.sizeToFreq) {
+                getMax();
+                GeneratorAndAnalyzer.mapIsUpdated = false;
+                GeneratorAndAnalyzer.sizeToFreq.notify();
+            }
+        }
+    });
 
     public static void main(String[] args) {
+        currentHigh.setDaemon(true);
         final List<Thread> analyzers = new ArrayList<>();
         ThreadGroup threads = new ThreadGroup("threads");
         for (int i = 0; i < QUANTITY_OF_ROUTS_VARIANTS; i++) {
             analyzers.add(new Thread(threads, () -> {
                 String newRout = GeneratorAndAnalyzer.generateRoute(LETTERS, LENGTHS_OF_THE_ROUT);
                 GeneratorAndAnalyzer.analyzeRoute(newRout, LETTER_FOR_ANALYSIS);
-
             }));
             analyzers.get(i).start();
         }
+
+        currentHigh.start();
+
         while (threads.activeCount() > 0) {
         }
+        System.out.println("Другие размеры:");
+        for (Map.Entry<Integer, Integer> entry : GeneratorAndAnalyzer.sizeToFreq.entrySet()) {
+            System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
+        }
+    }
+
+    static void getMax() {
         Map.Entry<Integer, Integer> entryMax = null;
         for (Map.Entry<Integer, Integer> entry : GeneratorAndAnalyzer.sizeToFreq.entrySet()) {
             if (entryMax == null) {
@@ -31,10 +51,6 @@ public class Main {
                 entryMax = entry;
             }
         }
-        System.out.println("Самое частое количество повторений" + entryMax.getKey() + "(встретилось " + entryMax.getValue() + " раз)\n" +
-                           "Другие размеры:");
-        for (Map.Entry<Integer, Integer> entry : GeneratorAndAnalyzer.sizeToFreq.entrySet()) {
-            System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
-        }
+        System.out.println("Самое частое количество повторений на данный момет " + entryMax.getKey() + "(встретилось " + entryMax.getValue() + " раз)");
     }
 }
